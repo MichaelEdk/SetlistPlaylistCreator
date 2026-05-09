9712851142afa3639f2678de34573d3608f151f6
MICHAELPC\Michael <michael.edkins@gmail.com>
2026-05-09 14:24:01 +1200

    feat: minor adjustment to button display

--- src/SetlistPlaylistCreator.Wpf/ArtistSearch/DesignerSupport/DesignTimeArtistSearchViewModel.cs
+++ src/SetlistPlaylistCreator.Wpf/ArtistSearch/DesignerSupport/DesignTimeArtistSearchViewModel.cs
@@ -41,6 +41,105 @@ namespace SetlistPlaylistCreator.Wpf.ArtistSearch.DesignerSupport
                             ]
                         )
                     ]
+                ),
+                new Setlist(
+                    Name: "Shea Stadium, New York, NY",
+                    ArtistName: "The Beatles",
+                    Sets:
+                    [
+                        new SetlistSet(
+                            Name: "Set 1",
+                            Songs:
+                            [
+                                new SetlistSong("Twist and Shout", "The Beatles"),
+                                new SetlistSong("She's a Woman", "The Beatles"),
+                                new SetlistSong("I Feel Fine", "The Beatles"),
+                                new SetlistSong("Dizzy Miss Lizzy", "The Beatles"),
+                                new SetlistSong("Ticket to Ride", "The Beatles"),
+                                new SetlistSong("Everybody's Trying to Be My Baby", "The Beatles"),
+                                new SetlistSong("Can't Buy Me Love", "The Beatles"),
+                                new SetlistSong("Baby's in Black", "The Beatles"),
+                                new SetlistSong("Act Naturally", "The Beatles"),
+                                new SetlistSong("A Hard Day's Night", "The Beatles"),
+                                new SetlistSong("Help!", "The Beatles"),
+                                new SetlistSong("I'm Down", "The Beatles")
+                            ]
+                        )
+                    ]
+                ),
+                new Setlist(
+                    Name: "Abbey Road Studio Sessions",
+                    ArtistName: "The Beatles",
+                    Sets:
+                    [
+                        new SetlistSet(
+                            Name: "Studio Recording",
+                            Songs:
+                            [
+                                new SetlistSong("Come Together", "The Beatles"),
+                                new SetlistSong("Something", "The Beatles"),
+                                new SetlistSong("Maxwell's Silver Hammer", "The Beatles"),
+                                new SetlistSong("Oh! Darling", "The Beatles"),
+                                new SetlistSong("Octopus's Garden", "The Beatles"),
+                                new SetlistSong("I Want You (She's So Heavy)", "The Beatles"),
+                                new SetlistSong("Here Comes the Sun", "The Beatles")
+                            ]
+                        )
+                    ]
+                ),
+                new Setlist(
+                    Name: "Hollywood Bowl, Los Angeles, CA",
+                    ArtistName: "The Beatles",
+                    Sets:
+                    [
+                        new SetlistSet(
+                            Name: "Main Set",
+                            Songs:
+                            [
+                                new SetlistSong("Rock and Roll Music", "Chuck Berry"),
+                                new SetlistSong("She's a Woman", "The Beatles"),
+                                new SetlistSong("If I Needed Someone", "The Beatles"),
+                                new SetlistSong("Day Tripper", "The Beatles"),
+                                new SetlistSong("Baby's in Black", "The Beatles"),
+                                new SetlistSong("I Feel Fine", "The Beatles"),
+                                new SetlistSong("Yesterday", "The Beatles"),
+                                new SetlistSong("I Wanna Be Your Man", "The Beatles"),
+                                new SetlistSong("Nowhere Man", "The Beatles"),
+                                new SetlistSong("Paperback Writer", "The Beatles"),
+                                new SetlistSong("Long Tall Sally", "Little Richard")
+                            ]
+                        )
+                    ]
+                ),
+                new Setlist(
+                    Name: "Budokan Hall, Tokyo, Japan",
+                    ArtistName: "The Beatles",
+                    Sets:
+                    [
+                        new SetlistSet(
+                            Name: "Set 1",
+                            Songs:
+                            [
+                                new SetlistSong("Rock and Roll Music", "Chuck Berry"),
+                                new SetlistSong("She's a Woman", "The Beatles"),
+                                new SetlistSong("If I Needed Someone", "The Beatles"),
+                                new SetlistSong("Day Tripper", "The Beatles"),
+                                new SetlistSong("Baby's in Black", "The Beatles")
+                            ]
+                        ),
+                        new SetlistSet(
+                            Name: "Set 2",
+                            Songs:
+                            [
+                                new SetlistSong("I Feel Fine", "The Beatles"),
+                                new SetlistSong("Yesterday", "The Beatles"),
+                                new SetlistSong("I Wanna Be Your Man", "The Beatles"),
+                                new SetlistSong("Nowhere Man", "The Beatles"),
+                                new SetlistSong("Paperback Writer", "The Beatles"),
+                                new SetlistSong("I'm Down", "The Beatles")
+                            ]
+                        )
+                    ]
                 )
             ];
         }
